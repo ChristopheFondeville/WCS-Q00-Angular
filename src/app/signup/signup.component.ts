710809cd5286d2455331cf9371ebab9f203d4346
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-09-26 21:39:33 +0200

    User Model

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User} from "../models/user";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent {
 
+  model: User = new User("","","","");
+
   onSubmit(): void {
     // form submitted
-    console.log('Formulaire envoyé !');
+    console.log(this.model);
   }
 
 }
